5e4084f238f7b3e818dfce5c084701f0c9a3e1bd
Gorgeeva Alexandra <sashagordeewa2001@gmail.com>
2024-05-02 20:29:51 +0500

    Код 5 занятия. redux toolkit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Products from './components/Products/Products';
 import logo123 from './assets/img/logo.png';
 import { Route, Routes } from 'react-router-dom';
 import Cart from './components/Cart/Cart';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/counterSlice';
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
       <Header />
       <Routes>
         <Route path="/" element={<Products />} />
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,28 +2,55 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from './../ProductCard/ProductCard';
 import style from './Products.module.css';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts } from '../../redux/productsSlice';
 
 const Products = () => {
-  // let products = [
-  //   { id: 1, name: 'Кексы', price: '100' },
-  //   { id: 2, name: 'Пирожное', price: '200' },
-  //   { id: 3, name: 'Торт', price: '150' },
-  //   { id: 4, name: 'Кекс', price: '300' },
-  // ];
+  //const [products, setProducts] = useState([]);
 
-  const [products, setProducts] = useState([]);
+  const products = useSelector((state) => state.products.products);
+
+  console.log(products);
+
+  const dispatch = useDispatch();
+
+  const fetchItems = () => {
+    axios.get('https://662a6f3467df268010a3eb19.mockapi.io/items').then((res) => {
+      dispatch(setProducts(res.data));
+    });
+  };
 
   useEffect(() => {
-    axios.get('https://662a6f3467df268010a3eb19.mockapi.io/items').then((res) => {
-      setProducts(res.data);
-    });
+    fetchItems();
   }, []);
 
+  const AddProduct = () => {
+    axios
+      .post(
+        'https://662a6f3467df268010a3eb19.mockapi.io/items',
+        {
+          id: '6',
+          name: 'Пончики',
+          price: 50,
+          image:
+            'https://img.povar.ru/mobile/0d/5c/7e/9d/ponchiki_na_kefire_za_15_minut-272904.JPG',
+        },
+        { 'content-type': 'application/json' },
+      )
+      .then((res) => {
+        console.log(res.data);
+        fetchItems();
+      });
+  };
+
   return (
-    <div className={style.items}>
-      {products.map((product) => (
-        <ProductCard key={product.id} {...product} />
-      ))}
+    <div>
+      <button onClick={AddProduct}>ADD</button>
+      <div className={style.items}>
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
     </div>
   );
 };
