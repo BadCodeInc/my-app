113cc4eb9ff8fc1bbb8b5139e5cf29c94a45891d
Gorgeeva Alexandra <sashagordeewa2001@gmail.com>
2024-05-23 19:34:12 +0500

    Код 8 занятия

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,12 @@ const Header = () => {
 
   const totalCount = items.reduce((sum, item) => sum + item.count, 0);
 
+  React.useEffect(() => {
+    localStorage.setItem('cartProducts', JSON.stringify(items));
+    localStorage.setItem('totalPrice', JSON.stringify(totalPrice));
+    console.log('LS изменен');
+  }, [items]);
+
   return (
     <div className={style.item}>
       <div>
